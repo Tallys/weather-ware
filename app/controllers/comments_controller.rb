eb0ee62f312230be4f0ad7f659356502564c93a7
Natalya Shelburne <natalyathree@gmail.com>
2014-12-03 20:47:51 -0600

    performed a miracle and got everything loading without refreshing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,12 @@
 class CommentsController < ApplicationController
 
 	def create
-		@location = Location.find(params[:location_id])
+		@location = Location.find(params[:id])
 		@comment = @location.comments.create(comment_params)
-		redirect_to location_path(@location)
+		redirect_to root_url(location: @location)
+	
 	end
 
-	def destroy
-		@location = Location.find(params[:location_id])
-		@comment = @location.comments.find(params[:id])
-		@comment.destroy
-		redirect_to location_path(@location)
-	end
 
 	private 
 		def comment_params
