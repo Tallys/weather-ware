ef87399ca75cca4484dfa74bef207c67e72c159b
Natalya Shelburne <natalyathree@gmail.com>
2014-12-06 17:14:13 -0600

    changed styles to make location query in the header

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -42,16 +42,3 @@ $(document).ready(function() {
 	})
 });
 
-
-
-// jQuery(document).ready(function($) {
-// 	$.ajax({
-// 		url : "http://api.wunderground.com/api/9f9665f04fbd9ca7/geolookup/conditions/q/"+state+"/"+city+".json",
-// 		dataType : "jsonp",
-// 		success : function(parsed_json) {
-// 			var location = parsed_json['location']['city'];
-// 			var temp_f = parsed_json['current_observation']['temp_f'];
-// 			alert("Current temperature in " + location + " is: " + temp_f);
-// 		}
-// 	});
-// });
